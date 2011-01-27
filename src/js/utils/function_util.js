c04089b33243fc816706851da6cd722f6995bec6
gdavis <grant.davis@gmail.com>
2011-01-26 17:12:33 -0700

    added feature to the function_util to maintain references to context-scoped methods that are created with the context() method call. this fixes a bug where when you tried to remove the original method handler, calling context() again would return a fresh new function pointer instead of the original.

--- src/js/utils/function_util.js
+++ src/js/utils/function_util.js
@@ -1,10 +1,18 @@
 // Allows for binding context to functions
 // when using in event listeners and timeouts
 // ! From video.js
+// Modified to maintain singular references to context-scoped methods so you can remove the listener later.
+Function.prototype.contextMethods = {};
 Function.prototype.context = function(obj){
-  var method = this,
-  temp = function(){
-    return method.apply(obj, arguments);
-  };
-  return temp;
+  var method = this;
+  if (Function.contextMethods[obj]) {
+    return Function.contextMethods[obj];
+  }
+  else {
+    var temp = function() {
+      return method.apply(obj, arguments);
+    };
+    Function.contextMethods[obj] = temp;
+    return temp;
+  }
 };
